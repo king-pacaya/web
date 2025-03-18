94ff833be6486b87a406bfb724aaedd750ce05d3
King Pacaya <83373501+king-pacaya@users.noreply.github.com>
2025-03-17 21:44:14 -0500

    Add files via upload

---
+++ topocaj/pages/contacto/javascript.js
@@ -0,0 +1,10 @@
+//Nav
+	//Responsive Dropdown
+		function myFunction() {
+		  var x = document.getElementById("myTopnav");
+		  if (x.className === "topnav") {
+		    x.className += " responsive";
+		  } else {
+		    x.className = "topnav";
+		  }
+		}
